71efffccac3273e6c323942557323d2a97689d51
yuemengrui <1401190335@qq.com>
2024-03-04 13:59:27 +0800

    update

--- ModelWorker/configs/baichuan2_13b_configs.py
+++ ModelWorker/configs/baichuan2_13b_configs.py
@@ -12,17 +12,17 @@ WORKER_ID = str(uuid.uuid4())[:8]
 ########################
 
 ModelWorkerConfig = {
-    "controller_addr": "http://xxx",
-    "worker_addr": f"http://xxx:{FASTAPI_PORT}",
+    "controller_addr": "http://model_controller:24620",
+    "worker_addr": f"http://model_worker_baichuan2_13b_server:{FASTAPI_PORT}",
     "worker_id": WORKER_ID,
     "worker_type": "",  # vllm or others
-    "model_type": "xx",  # ["Baichuan"]
-    "model_path": "xxx",
-    "model_name": "xxx",
+    "model_type": "Baichuan",  # ["Baichuan"]
+    "model_path": "/workspace/Models/Baichuan2-13B-Chat",
+    "model_name": "Baichuan2_13B",
     "limit_worker_concurrency": 5,
     "multimodal": False,
     "device": "cuda",
-    "dtype": "float16",  # ["float32", "float16", "bfloat16"],
+    "dtype": "bfloat16",  # ["float32", "float16", "bfloat16"],
     "gpu_memory_utilization": 0.9
 }
 
