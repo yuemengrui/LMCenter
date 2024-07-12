66cfa69145b8e6ceb89a482a19baca89007a3e74
yuemengrui <1401190335@qq.com>
2024-07-12 15:39:20 +0800

    update redoc url

--- ModelController/info/__init__.py
+++ ModelController/info/__init__.py
@@ -38,7 +38,7 @@ def app_registry(app):
         logger.info(f'end request "{request.method} {request.url.path}" - {cost:.3f}s')
         return response
 
-    app.mount("/static", StaticFiles(directory=f"static"), name="static")
+    app.mount("/ai/llm/static", StaticFiles(directory=f"static"), name="static")
 
     @app.get("/ai/llm/docs", include_in_schema=False)
     async def custom_swagger_ui_html():
