4a298f981fc493288c110e46ea361686857ca88e
yuemengrui <1401190335@qq.com>
2024-07-12 15:22:37 +0800

    update redoc url

--- ModelController/info/__init__.py
+++ ModelController/info/__init__.py
@@ -43,11 +43,11 @@ def app_registry(app):
     @app.get("/ai/llm/docs", include_in_schema=False)
     async def custom_swagger_ui_html():
         return get_swagger_ui_html(
-            openapi_url=app.openapi_url,
+            openapi_url="/ai/llm/openapi.json",
             title=app.title + " - Swagger UI",
             oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-            swagger_js_url="/static/swagger-ui-bundle.js",
-            swagger_css_url="/static/swagger-ui.css",
+            swagger_js_url="/ai/llm/static/swagger-ui-bundle.js",
+            swagger_css_url="/ai/llm/static/swagger-ui.css",
         )
 
     @app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
@@ -57,9 +57,9 @@ def app_registry(app):
     @app.get("/ai/llm/redoc", include_in_schema=False)
     async def redoc_html():
         return get_redoc_html(
-            openapi_url=app.openapi_url,
+            openapi_url="/ai/llm/openapi.json",
             title=app.title + " - ReDoc",
-            redoc_js_url="/static/redoc.standalone.js",
+            redoc_js_url="/ai/llm/static/redoc.standalone.js",
         )
 
     from info.modules import register_router
